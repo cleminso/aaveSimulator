155518746be1534a786cb8b6f810b07299650daf
cleminso <clem2inso@gmail.com>
2025-01-25 17:38:15 +0700

    error fixing

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-import {
-  AudioWaveform,
-  Command,
-  GalleryVerticalEnd,
-  SquareTerminal,
-} from "lucide-react";
+import { SquareTerminal } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { NavMain } from "@/components/nav-main";
