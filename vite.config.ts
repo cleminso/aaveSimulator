fc4f20d0b9d603377e18f5ad1a71b5890fccb989
cleminso <clem2inso@gmail.com>
2025-02-02 18:10:54 +0700

    setup testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-import path from "path";
+import react from "@vitejs/plugin-react";
+import type { UserConfigExport } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
-});
+}) satisfies UserConfigExport;
