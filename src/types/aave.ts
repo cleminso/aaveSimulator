816062c84ee4a3045409157a72aab69d105547f7
cleminso <clem2inso@gmail.com>
2025-02-06 19:43:10 +0700

    fix aave-parameter.ts

--- src/types/aave.ts
+++ src/types/aave.ts
@@ -2,9 +2,8 @@ export interface AaveParameter {
   name: string;
   "Collateral Enabled": boolean;
   "Borrowing Enabled": boolean;
-  LTV: string;
   "Liquidation Threshold": string;
-  "Optimal Utilization": string;
+  LTV: string;
   "Variable Borrow Rate": string;
 }
 
