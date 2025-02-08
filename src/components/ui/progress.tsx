89f6bca5e19f4f0d086f74b0a0b73560ba74899a
cleminso <clem2inso@gmail.com>
2025-02-08 17:20:19 +0700

    style: Change progress indicator background to amber

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,7 +5,9 @@ import { cn } from "@/libs/utils";
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { indicatorClassName?: string }
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    indicatorClassName?: string;
+  }
 >(({ className, value, indicatorClassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -16,7 +18,11 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 transition-all", indicatorClassName, "bg-primary")}
+      className={cn(
+        "h-full w-full flex-1 transition-all",
+        indicatorClassName,
+        "bg-amber-300",
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
