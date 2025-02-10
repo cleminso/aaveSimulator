02279105bcce89b3809a6b4bdebf960487cfbcdd
cleminso <clem2inso@gmail.com>
2025-02-10 14:52:41 +0700

    remove user-address-input

--- src/components/main-section.tsx
+++ src/components/main-section.tsx
@@ -1,14 +1,8 @@
-import { Input } from "@/components/ui/input";
 import { CollateralVsDebt } from "./collateral-vs-debt";
 
 export function MainSection() {
   return (
     <div className="flex flex-col gap-6">
-      <Input
-        type="text"
-        className="w-1/3 text-base"
-        placeholder="Enter your Ethereum address"
-      />
       <CollateralVsDebt />
     </div>
   );
